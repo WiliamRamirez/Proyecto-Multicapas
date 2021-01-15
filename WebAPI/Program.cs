cb1583c044a18f39b77ec40cea57fde242eb6263
WiliamRamirez <wiliam.ramirez.27@unsch.edu.pe>
2021-01-15 01:06:41 -0500

    Agregando Entity-IdentityDbContext

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -2,10 +2,15 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Domain.Entities;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Persistence;
 
 namespace WebAPI
 {
@@ -13,7 +18,33 @@ namespace WebAPI
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var hostserver = CreateHostBuilder(args).Build();
+
+            using (var ambiente = hostserver.Services.CreateScope())
+            {
+                var services = ambiente.ServiceProvider;
+
+                try
+                {
+                    // Agregar data de prueba
+                    var userManager = services.GetRequiredService<UserManager<Usuario>>();
+
+                    var context = services.GetRequiredService<SistemaDbContext>();
+                    context.Database.Migrate();
+
+
+                    // DataPrueba.InsertarData(context, userManager).Wait(); // Wait() -> para hacerlo asincrono
+                }
+                catch (Exception e)
+                {
+
+                    var logging = services.GetRequiredService<ILogger<Program>>();
+                    logging.LogError(e, "Ocurrio un error en la migracion");
+                }
+
+                hostserver.Run();
+
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -2,14 +2,20 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Domain.Entities;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Persistence;
 
 namespace WebAPI
 {
@@ -25,6 +31,23 @@ namespace WebAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // Coneccion a la base de datos
+            services.AddDbContext<SistemaDbContext>(opt =>
+            {
+                opt.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+            });
+
+
+            // Agregando IdentityFrameworkCore a webAPI
+            var builder = services.AddIdentityCore<Usuario>();
+            var identityBuilder = new IdentityBuilder(builder.UserType, builder.Services);
+            identityBuilder.AddRoles<IdentityRole>();
+            identityBuilder.AddClaimsPrincipalFactory<UserClaimsPrincipalFactory<Usuario, IdentityRole>>();
+            identityBuilder.AddEntityFrameworkStores<SistemaDbContext>();
+            identityBuilder.AddSignInManager<SignInManager<Usuario>>();
+            services.TryAddSingleton<ISystemClock, SystemClock>();
+            
+
             services.AddControllers();
         }
 
